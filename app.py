74ac3d7738aef250f8575b5672f2086884f509cb
MSriVarshini <mukkavillisrivarshini@gmail.com>
2026-08-30 22:32:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,11 @@ app.secret_key = os.getenv("SECRET_KEY", "focusease_secret")
 # --------------------------------------------------
 
 db_config = {
-    "host": os.getenv("MYSQL_HOST"),
-    "user": os.getenv("MYSQL_USER"),
-    "password": os.getenv("MYSQL_PASSWORD"),
-    "database": os.getenv("MYSQL_DATABASE")
+   "host": os.getenv("MYSQLHOST") or os.getenv("MYSQL_HOST"),
+    "user": os.getenv("MYSQLUSER") or os.getenv("MYSQL_USER"),
+    "password": os.getenv("MYSQLPASSWORD") or os.getenv("MYSQL_PASSWORD"),
+    "database": os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DATABASE"),
+    "port": int(os.getenv("MYSQLPORT") or os.getenv("MYSQL_PORT") or 3306)
 }
 
 # --------------------------------------------------
@@ -53,7 +54,10 @@ persona_names = {
 # --------------------------------------------------
 
 def get_db_connection():
-    return mysql.connector.connect(**db_config)
+    return mysql.connector.connect(
+        **db_config,
+        connection_timeout=10
+    )
 
 
 # --------------------------------------------------
